f400ff324e1737bacdb02b29257d1799867acbd9
Lydiaade <lydiaadejumo@outlook.com>
2018-12-06 00:45:02 +0000

    Updating Contact form response

--- Home/Home.py
+++ Home/Home.py
@@ -19,20 +19,20 @@ def Home():
         return redirect(url_for('Decide'))
     return render_template('Home.html')
 
-#@app.route("/signup", methods=["POST"])
-#def sign_up():
-#    form_data = request.form
-#    print form_data["email"]
-#    send_simple_message(form_data["email"])
-#    return "We'll get back to you soon."
+@app.route("/contact", methods=["POST"])
+def Contact():
+    form_data = request.form
+    print form_data["email"]
+    send_simple_message(form_data["email"])
+    return render_template('Contact.html')
 
-#def send_simple_message(email):
-#    return requests.post(
-#        "https://api.mailgun.net/v3/sandboxa9ce70879809408aa3386c574ebdf5e6.mailgun.org/messages",
-#        auth=("api", "d470b0501ac7d51991d94c5c43b52951-059e099e-c9fa3bca"),
-#        data={"from": "Welcome trial<mailgun@sandboxa9ce70879809408aa3386c574ebdf5e6.mailgun.org>",
-#              "to": [email],
-#              "subject": "Hello",
-#              "text": "Thanks for joining the team you will soon be able to hear more from blogs in the upcoming weeks."})
+def send_simple_message(email):
+    return requests.post(
+        "https://api.mailgun.net/v3/sandboxa9ce70879809408aa3386c574ebdf5e6.mailgun.org/messages",
+        auth=("api", "d470b0501ac7d51991d94c5c43b52951-059e099e-c9fa3bca"),
+        data={"from": "Decisions 1.0<mailgun@sandboxa9ce70879809408aa3386c574ebdf5e6.mailgun.org>",
+              "to": [email],
+              "subject": "Contact Response",
+              "text": "Thank for your expressed interest. We will definetely take your opinions on board when improving Decisions 1.0. In order to register your query please email <insert email> with your issue and will take it from there. Once again, thank you for your expressed interest and have a lovely day."})
 
 app.run(debug=True)
