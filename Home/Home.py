52ad48a7bc993015d8e72a0c146da66dbc99cf53
Lydia <lydiaadejumo@outlook.com>
2019-11-06 17:09:06 +0000

    Reworking website
    Update to python 3

--- Home/Home.py
+++ Home/Home.py
@@ -12,27 +12,27 @@ def Decide():
     if request.method == 'POST':
         return redirect(url_for('Homepage'))
     del main_name[:]
-    print main_name
+    print(main_name)
     del reasoning[:]
-    print reasoning
+    print(reasoning)
     del options_list[:]
-    print options_list
+    print(options_list)
     del variables_list[:]
-    print variables_list
+    print(variables_list)
     del main_ranks_list[:]
-    print main_ranks_list
+    print(main_ranks_list)
     del option1_ranks[:]
-    print option1_ranks
+    print(option1_ranks)
     del results_for_all_options[:]
-    print results_for_all_options
+    print(results_for_all_options)
     del option2_ranks[:]
-    print option2_ranks
+    print(option2_ranks)
     del option3_ranks[:]
-    print option3_ranks
+    print(option3_ranks)
     del ordered_final_list[:]
-    print ordered_final_list
+    print(ordered_final_list)
     del corr_ordered_final_list[:]
-    print corr_ordered_final_list
+    print(corr_ordered_final_list)
     return render_template('Decide.html')
 
 @app.route('/Howitworks', methods=['GET', 'POST'])
@@ -50,7 +50,7 @@ def Home():
 @app.route("/contact", methods=["POST"])
 def Contact():
     form_data = request.form
-    print form_data["email"]
+    print(form_data["email"])
     send_simple_message(form_data["email"])
     return render_template('Contact.html')
 
@@ -70,9 +70,9 @@ def Decide2():
     form_data = request.form
     main_name.append(form_data["name"])
     reasoning.append(form_data["reason"])
-    print form_data["name"]
-    print main_name
-    print reasoning
+    print(form_data["name"])
+    print(main_name)
+    print(reasoning)
     return render_template('Decide2.html', name=request.form["name"])
 
 options_list=[]
@@ -80,13 +80,13 @@ options_list=[]
 @app.route("/decide3", methods=["POST"])
 def Decide3():
     form_data = request.form
-    print form_data["option1"]
-    print form_data["option2"]
-    print form_data["option3"]
+    print(form_data["option1"])
+    print(form_data["option2"])
+    print(form_data["option3"])
     options_list.append(form_data["option1"])
     options_list.append(form_data["option2"])
     options_list.append(form_data["option3"])
-    print options_list
+    print(options_list)
     return render_template('Decide3.html')
 
 variables_list=[]
@@ -94,13 +94,13 @@ variables_list=[]
 @app.route("/decide4", methods=["POST"])
 def Decide4():
     form_data = request.form
-    print form_data["variable1"]
-    print form_data["variable2"]
-    print form_data["variable3"]
+    print(form_data["variable1"])
+    print(form_data["variable2"])
+    print(form_data["variable3"])
     variables_list.append(form_data["variable1"])
     variables_list.append(form_data["variable2"])
     variables_list.append(form_data["variable3"])
-    print variables_list
+    print(variables_list)
     return render_template('Decide4.html', variable1=request.form["variable1"], variable2=request.form["variable2"], variable3=request.form["variable3"])
 
 main_ranks_list=[]
@@ -108,13 +108,13 @@ main_ranks_list=[]
 @app.route("/rankorder1", methods=["POST"])
 def Rankorder1():
     form_data = request.form
-    print form_data["rank1"]
-    print form_data["rank2"]
-    print form_data["rank3"]
+    print(form_data["rank1"])
+    print(form_data["rank2"])
+    print(form_data["rank3"])
     main_ranks_list.append(form_data["rank1"])
     main_ranks_list.append(form_data["rank2"])
     main_ranks_list.append(form_data["rank3"])
-    print main_ranks_list
+    print(main_ranks_list)
     return render_template('RankOrdering1.html', variable1=variables_list[0], variable2=variables_list[1], variable3=variables_list[2], option1=options_list[0])
 
 option1_ranks=[]
@@ -123,17 +123,17 @@ results_for_all_options=[]
 @app.route("/rankorder2", methods=["POST"])
 def Rankorder2():
     form_data = request.form
-    print form_data["opt1rank1"]
-    print form_data["opt1rank2"]
-    print form_data["opt1rank3"]
+    print(form_data["opt1rank1"])
+    print(form_data["opt1rank2"])
+    print(form_data["opt1rank3"])
     option1_ranks.append(form_data["opt1rank1"])
     option1_ranks.append(form_data["opt1rank2"])
     option1_ranks.append(form_data["opt1rank3"])
-    print option1_ranks
+    print(option1_ranks)
     result1= (4-float(main_ranks_list[0]))*float(option1_ranks[0]) + (4-float(main_ranks_list[1]))*float(option1_ranks[1]) + (4-float(main_ranks_list[2]))*float(option1_ranks[2])
-    print result1
+    print(result1)
     results_for_all_options.append(result1)
-    print results_for_all_options
+    print(results_for_all_options)
     return render_template('RankOrdering2.html', variable1=variables_list[0], variable2=variables_list[1], variable3=variables_list[2], option2=options_list[1])
 
 
@@ -142,17 +142,17 @@ option2_ranks=[]
 @app.route("/rankorder3", methods=["POST"])
 def Rankorder3():
     form_data = request.form
-    print form_data["opt2rank1"]
-    print form_data["opt2rank2"]
-    print form_data["opt2rank3"]
+    print(form_data["opt2rank1"])
+    print(form_data["opt2rank2"])
+    print(form_data["opt2rank3"])
     option2_ranks.append(form_data["opt2rank1"])
     option2_ranks.append(form_data["opt2rank2"])
     option2_ranks.append(form_data["opt2rank3"])
-    print option2_ranks
+    print(option2_ranks)
     result2= (4-float(main_ranks_list[0]))*float(option2_ranks[0]) + (4-float(main_ranks_list[1]))*float(option2_ranks[1]) + (4-float(main_ranks_list[2]))*float(option2_ranks[2])
-    print result2
+    print(result2)
     results_for_all_options.append(result2)
-    print results_for_all_options
+    print(results_for_all_options)
     return render_template('RankOrdering3.html', variable1=variables_list[0], variable2=variables_list[1], variable3=variables_list[2], option3=options_list[2])
 
 option3_ranks=[]
@@ -160,22 +160,22 @@ option3_ranks=[]
 @app.route("/finaldecision", methods=["POST"])
 def finaldecision():
     form_data = request.form
-    print form_data["opt3rank1"]
-    print form_data["opt3rank2"]
-    print form_data["opt3rank3"]
+    print(form_data["opt3rank1"])
+    print(form_data["opt3rank2"])
+    print(form_data["opt3rank3"])
     option3_ranks.append(form_data["opt3rank1"])
     option3_ranks.append(form_data["opt3rank2"])
     option3_ranks.append(form_data["opt3rank3"])
-    print option3_ranks
+    print(option3_ranks)
     result3= (4-float(main_ranks_list[0]))*float(option3_ranks[0]) + (4-float(main_ranks_list[1]))*float(option3_ranks[1]) + (4-float(main_ranks_list[2]))*float(option3_ranks[2])
-    print result3
+    print(result3)
     results_for_all_options.append(result3)
-    print results_for_all_options
+    print(results_for_all_options)
     final_conclusion1()
     final_conclusion2()
     final_conclusion3()
-    print ordered_final_list
-    print corr_ordered_final_list
+    print(ordered_final_list)
+    print(corr_ordered_final_list)
     return render_template('FINAL.html', name=main_name[0], reason=reasoning[0], option1=ordered_final_list[0], option1point=corr_ordered_final_list[0], option2=ordered_final_list[1], option2point=corr_ordered_final_list[1], option3=ordered_final_list[2], option3point=corr_ordered_final_list[2] )
 
 ordered_final_list=[]
@@ -184,77 +184,77 @@ def final_conclusion1():
     if (results_for_all_options[0]>results_for_all_options[1] and results_for_all_options[0]>results_for_all_options[2]):
         ordered_final_list.append(options_list[0])
         corr_ordered_final_list.append(results_for_all_options[0])
-        print "Winner is ",options_list[0],", with ",results_for_all_options[0],
-        print ordered_final_list
+        print("Winner is ",options_list[0],", with ",results_for_all_options[0],)
+        print(ordered_final_list)
     elif (results_for_all_options[1]>results_for_all_options[0] and results_for_all_options[1]>results_for_all_options[2]):
         ordered_final_list.append(options_list[1])
         corr_ordered_final_list.append(results_for_all_options[1])
-        print "Winner is ",options_list[1],", with ",results_for_all_options[1],
-        print ordered_final_list
+        print("Winner is ",options_list[1],", with ",results_for_all_options[1],)
+        print(ordered_final_list)
     else:
         ordered_final_list.append(options_list[2])
         corr_ordered_final_list.append(results_for_all_options[2])
-        print "Winner is ",options_list[2],", with ",results_for_all_options[2],
-        print ordered_final_list
+        print("Winner is ",options_list[2],", with ",results_for_all_options[2],)
+        print(ordered_final_list)
 
 def final_conclusion2():
     if ((results_for_all_options[0]>results_for_all_options[1] and results_for_all_options[0]<results_for_all_options[2]) or (results_for_all_options[0]<results_for_all_options[1] and results_for_all_options[0]>results_for_all_options[2])):
         ordered_final_list.append(options_list[0])
         corr_ordered_final_list.append(results_for_all_options[0])
-        print "Second is ",options_list[0],", with ",results_for_all_options[0],
-        print ordered_final_list
+        print("Second is ",options_list[0],", with ",results_for_all_options[0],)
+        print(ordered_final_list)
     elif ((results_for_all_options[1]>results_for_all_options[0] and results_for_all_options[1]<results_for_all_options[2]) or (results_for_all_options[1]<results_for_all_options[0] and results_for_all_options[1]>results_for_all_options[2])):
         ordered_final_list.append(options_list[1])
         corr_ordered_final_list.append(results_for_all_options[1])
-        print "Second is ",options_list[1],", with ",results_for_all_options[1],
-        print ordered_final_list
+        print("Second is ",options_list[1],", with ",results_for_all_options[1],)
+        print(ordered_final_list)
     else:
         ordered_final_list.append(options_list[2])
         corr_ordered_final_list.append(results_for_all_options[2])
-        print "Second is ",options_list[2],", with ",results_for_all_options[2],
-        print ordered_final_list
+        print("Second is ",options_list[2],", with ",results_for_all_options[2],)
+        print(ordered_final_list)
 
 def final_conclusion3():
     if (results_for_all_options[0]<results_for_all_options[1] and results_for_all_options[0]<results_for_all_options[2]):
         ordered_final_list.append(options_list[0])
         corr_ordered_final_list.append(results_for_all_options[0])
-        print "Last is ",options_list[0],", with ",results_for_all_options[0],
-        print ordered_final_list
+        print("Last is ",options_list[0],", with ",results_for_all_options[0],)
+        print(ordered_final_list)
     elif (results_for_all_options[1]<results_for_all_options[0] and results_for_all_options[1]<results_for_all_options[2]):
         ordered_final_list.append(options_list[1])
         corr_ordered_final_list.append(results_for_all_options[1])
-        print "Last is ",options_list[1],", with ",results_for_all_options[1],
-        print ordered_final_list
+        print("Last is ",options_list[1],", with ",results_for_all_options[1],)
+        print(ordered_final_list)
     else:
         ordered_final_list.append(options_list[2])
         corr_ordered_final_list.append(results_for_all_options[2])
-        print "Last is ",options_list[2],", with ",results_for_all_options[2],
-        print ordered_final_list
+        print("Last is ",options_list[2],", with ",results_for_all_options[2],)
+        print(ordered_final_list)
 
 @app.route("/restart", methods=["POST"])
 def restart():
     del main_name[:]
-    print main_name
+    print(main_name)
     del reasoning[:]
-    print reasoning
+    print(reasoning)
     del options_list[:]
-    print options_list
+    print(options_list)
     del variables_list[:]
-    print variables_list
+    print(variables_list)
     del main_ranks_list[:]
-    print main_ranks_list
+    print(main_ranks_list)
     del option1_ranks[:]
-    print option1_ranks
+    print(option1_ranks)
     del results_for_all_options[:]
-    print results_for_all_options
+    print(results_for_all_options)
     del option2_ranks[:]
-    print option2_ranks
+    print(option2_ranks)
     del option3_ranks[:]
-    print option3_ranks
+    print(option3_ranks)
     del ordered_final_list[:]
-    print ordered_final_list
+    print(ordered_final_list)
     del corr_ordered_final_list[:]
-    print corr_ordered_final_list
+    print(corr_ordered_final_list)
     return render_template('Decide.html')
 
 
